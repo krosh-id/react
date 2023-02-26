3060215785172aec2aae7468b2f115b379e629a8
Vlad <87599468+krosh-id@users.noreply.github.com>
2023-02-26 12:26:23 +0300

    обновление структуры
    созданы свои компоненты, сервис для получения постов юзеров, стили (находятся в доработке)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-// import './mycss.css'
-// import * as classstyle from './mycs.module.css'
+
+import './styles/App.css';
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Row } from 'antd'
+import MyCard from './components/UI/MyCard/MyCard';
+
 
 function App() {
-  //const [counter, setCounter] = useState()
+
   const [users, setUsers] = useState([])
   const [userPosts, setPosts] = useState([])
 
-  // const increment = () => {
-  //   setCounter(counter+1)
-  // }
-  // const decrement = () => {
-  //   if (counter > 0) setCounter(counter-1)
-    
-  // }
+
 
   const getData = () => {
 
@@ -39,64 +32,34 @@ function App() {
   }
 
 
-  const loadUsers = () => {
-    getData()
-  }
+  // const loadUsers = () => {
+  //   getData()
+  // }
 
   useEffect(()=>{
     getData()
-    
   }, [])
 
   
 
   const styles = {
-    // 'border: 1 solid #000',
-    // color: 'red',
-    border: '1px solid #F5F5F5',
+    border: '1px solid #F5F5F5', //добавить в app.css
     padding: 10,
     margin: 'auto',
-    //borderRadius: 10,
+    
     marginBottom: 5,
-    backgroundColor: '#FFE4C4'
+    backgroundColor: '#FFFFFF'
   }
+
   return(
-    <>
+    <div className='App'>
+       
       <h2 style={styles}>Information about users</h2>
-      <div style={{margin: 50, gap: 16,}}>
-        {users.length > 0 &&
-          users.map(user => {
-            return (
-              
-              <Card title={user.name} key={Math.random()} style={{width: 600, margin:10 }} headStyle={{backgroundColor: '#FFE4C4'}}>
-              <p style={{fontFamily: 'monospace'}}>Email: {user.email}</p>
-              
-              <Row gutter={10}>  
-               { 
-                
-                userPosts.filter(post => post.userId === user.id).map(post =>{
-                  return(
-                    
-                    <Col span={30}>
-                    <Card title={post.title}  bordered={false} style={{margin:10}} headStyle={{backgroundColor: '#FFF8DC'}}>
-                    <p >{post.body} </p>
-                    </Card>
-                    </Col>
-                    
-                      ) 
-                
-                    } 
-                  )
-                
-              }
-              </Row>
-            </Card>
-            )
-            
-          })
-        }
-      </div>
-    </>
+      
+      
+      <MyCard users={users} userPosts={userPosts}/>
+      
+    </div>
   )
 }
 
